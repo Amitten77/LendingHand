82e977c2794fb58f82ba4058ace95949ec152ec4
Shray Srivastava <shray3420@gmail.com>
2023-10-21 19:56:40 -0500

    fixed up frontend

--- frontend/src/app/create/page.tsx
+++ frontend/src/app/create/page.tsx
@@ -1,101 +1,233 @@
-'use client'
-import React from 'react'
-import LendingHand from '../../../../backend/src/abis/LendingHand.json'
-import { useState, useEffect } from 'react';
-import Web3 from 'web3';
-import useAccount from '@/contexts/AuthContext';
-import BigNumber from 'bignumber.js';
+"use client";
+import React from "react";
+import LendingHand from "../../../../backend/src/abis/LendingHand.json";
+import { useState, useEffect } from "react";
+import Web3 from "web3";
+import useAccount from "@/contexts/AuthContext";
+import BigNumber from "bignumber.js";
 
-
-
-
+import { BeatLoader } from "react-spinners";
 
 const CreatePost = () => {
-
-
   const [loading, setLoading] = useState<boolean>(true);
-  const [formData, setFormData] = useState({ title: '', description: '', goal: '' });
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    goal: "",
+    deadline: "",
+    currency: "",
+  });
   const [lendinghand, setLendingHand] = useState<any>();
   const [web3, setWeb3] = useState<any>();
-  const { account } = useAccount()
+  const { account } = useAccount();
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
 
   useEffect(() => {
-    console.log(account)
-    console.log("HI")
+    console.log(account);
+    console.log("HI");
     const loadBlockchainData = async () => {
       const init_web3 = new Web3(window.ethereum);
       if (init_web3) {
-        setWeb3(init_web3)
+        setWeb3(init_web3);
       }
       //Load Account
-      const networkId = await window.ethereum?.request({ method: 'net_version' });
-      const networkData = LendingHand.networks[networkId as keyof typeof LendingHand.networks];
+      const networkId = await window.ethereum?.request({
+        method: "net_version",
+      });
+      const networkData =
+        LendingHand.networks[networkId as keyof typeof LendingHand.networks];
       if (networkData) {
-        const abi = LendingHand.abi
-        const address = networkData.address
-        console.log(abi, address)
-         const lendinghand = new init_web3.eth.Contract(abi, address)
-         setLendingHand(lendinghand);
-      setLoading(false)
-    }
-    else {
-        window.alert('Please connect your Metamask Account to the Goerli TestNet')
+        const abi = LendingHand.abi;
+        const address = networkData.address;
+        console.log(abi, address);
+        const lendinghand = new init_web3.eth.Contract(abi, address);
+        setLendingHand(lendinghand);
+        setLoading(false);
+      } else {
+        window.alert(
+          "Please connect your Metamask Account to the Goerli TestNet"
+        );
       }
-      
-    }
+    };
 
+    loadBlockchainData().catch(console.error);
+  }, []);
 
-    loadBlockchainData().catch(console.error)
-
-
-
-  }, [])
-
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
   const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    
-    console.log('Form submitted:', formData);
 
+    console.log("Form submitted:", formData);
 
-    lendinghand.methods.createPost(formData.title, formData.description, web3.utils.toWei(formData.goal, 'ether')).send({from: account[0] }).once('receipt', (receipt: any) => {
-      setLoading(false)
-      })
+    lendinghand.methods
+      .createPost(
+        formData.title,
+        formData.description,
+        web3.utils.toWei(formData.goal, "ether")
+      )
+      .send({ from: account[0] })
+      .once("receipt", (receipt: any) => {
+        setLoading(false);
+      });
 
-    setFormData({ title: '', description: '', goal: '' });
+    setFormData({
+      title: "",
+      description: "",
+      goal: "",
+      deadline: "",
+      currency: "",
+    });
 
-    setLoading(true)
-  }
+    setLoading(true);
+  };
 
-
-
-
-
-  return (
-    loading ? <p>Loading...</p> :
-    <form onSubmit={handleOnSubmit}>
-      <p>{account}</p>
-      <div className="form-group">
-        <label htmlFor="title">Title:</label>
-        <input type="text" id="title" name="title" value={formData.title} onChange={handleChange} required />
-      </div>
-      <div className="form-group">
-        <label htmlFor="description">Description:</label>
-        <input type="description" id="description" name="description" value={formData.description} onChange={handleChange} required />
-      </div>
-      <div className="form-group">
-        <label htmlFor="goal">Goal:</label>
-        <textarea id="goal" name="goal" value={formData.goal} onChange={handleChange} required />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-    
-  )
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            setImagePreview(reader.result as string);
+        };
+        reader.readAsDataURL(file);
+    }
 }
 
-export default CreatePost
+  return loading ? (
+    <div className="flex justify-center items-center min-h-screen">
+          <BeatLoader color="#3498db" size={25} />
+    </div>
+  ) : (
+    <div className="max-w-lg mx-auto mt-10 p-6 bg-white rounded-xl shadow-lg">
+      <form
+        onSubmit={handleOnSubmit}
+        className="space-y-6 bg-white p-6 rounded-md shadow-lg"
+      >
+        <h2 className="text-2xl font-bold mb-4 text-center">
+          Create Fundraiser
+        </h2>
+
+        <div>
+          <label
+            htmlFor="title"
+            className="block text-sm font-medium text-gray-600"
+          >
+            Title:
+          </label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            required
+            className="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-200 appearance-none transition duration-150"
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor="description"
+            className="block text-sm font-medium text-gray-600"
+          >
+            Description:
+          </label>
+          <textarea
+            id="description"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            required
+            className="mt-1 p-2 w-full border rounded-md h-24 focus:ring focus:ring-blue-200 appearance-none transition duration-150"
+          />
+        </div>
+
+        <div className="flex justify-between space-x-4">
+          <div className="w-1/2">
+            <label
+              htmlFor="goal"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Project Goal:
+            </label>
+            <input
+              type="number"
+              id="goal"
+              name="goal"
+              value={formData.goal}
+              onChange={handleChange}
+              required
+              className="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-200 appearance-none transition duration-150"
+            />
+          </div>
+          <div className="w-1/2">
+            <label
+              htmlFor="currency"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Currency:
+            </label>
+            <select
+              id="currency"
+              name="currency"
+              value={formData.currency}
+              onChange={handleChange}
+              className="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-200 appearance-none transition duration-150"
+            >
+              <option value="ETH">Ethereum (ETH)</option>
+              <option value="BTC">Bitcoin (BTC)</option>
+            </select>
+          </div>
+        </div>
+
+        <div>
+          <label
+            htmlFor="deadline"
+            className="block text-sm font-medium text-gray-600"
+          >
+            Deadline:
+          </label>
+          <input
+            type="date"
+            id="deadline"
+            name="deadline"
+            value={formData.deadline}
+            onChange={handleChange}
+            required
+            className="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-200 appearance-none transition duration-150"
+          />
+        </div>
+
+        <div>
+        <label htmlFor="image" className="block text-sm font-medium text-gray-600">Fundraiser Image:</label>
+        <input 
+            type="file" 
+            id="image" 
+            name="image" 
+            onChange={handleImageChange}
+            className="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-200 appearance-none transition duration-150 cursor-pointer"
+        />
+        {imagePreview && (
+            <div className="mt-2">
+                <img src={imagePreview} alt="Preview" className="w-full h-64 rounded-md object-cover"/>
+            </div>
+        )}
+    </div>
+
+        <button
+          type="submit"
+          className="w-full py-2 px-4 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-200 transform transition hover:scale-105 active:bg-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
--- frontend/src/app/lend/page.tsx
+++ frontend/src/app/lend/page.tsx
@@ -1,94 +1,103 @@
 "use client";
-import React from 'react'
-import LendingHand from '../../../../backend/src/abis/LendingHand.json'
-import { useState, useEffect } from 'react';
-import Web3 from 'web3';
-
+import React from "react";
+import LendingHand from "../../../../backend/src/abis/LendingHand.json";
+import { useState, useEffect } from "react";
+import Web3 from "web3";
+import { PulseLoader } from "react-spinners";
 
 type insideNetwork = {
   events: {};
   links: {};
   address: string;
   transactionHash: string;
-}
+};
 
 type postType = {
-  current: bigint
-  description: string
-  goal: bigint
-  id:  bigint
-  name: string
-  numDonors: bigint
-  owner: string
-  reachedGoal: boolean
-}
+  current: bigint;
+  description: string;
+  goal: bigint;
+  id: bigint;
+  name: string;
+  numDonors: bigint;
+  owner: string;
+  reachedGoal: boolean;
+};
 
 const Lend = () => {
-
-
-  const [posts, setPosts] = useState<any>([])
-  const [loading, setLoading] = useState<boolean>(true)
-
-
+  const [posts, setPosts] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const loadBlockchainData = async () => {
       const web3 = new Web3(window.ethereum);
       //Load Account
-      const networkId = await window.ethereum?.request({ method: 'net_version' });
-      const networkData = LendingHand.networks[networkId as keyof typeof LendingHand.networks];
+      const networkId = await window.ethereum?.request({
+        method: "net_version",
+      });
+      const networkData =
+        LendingHand.networks[networkId as keyof typeof LendingHand.networks];
       if (networkData) {
-        const abi = LendingHand.abi
-        const address = networkData.address
-        console.log(abi, address)
-        const lendinghand = new web3.eth.Contract(abi, address)
-        const postCount = await lendinghand.methods.postCount().call()
-        if (typeof postCount === 'bigint') {
-        for (var i = 1; i <= postCount; i++) {
-          const post = await lendinghand.methods.posts(i).call()
-          setPosts(posts => [...posts, post])
+        const abi = LendingHand.abi;
+        const address = networkData.address;
+        console.log(abi, address);
+        const lendinghand = new web3.eth.Contract(abi, address);
+        const postCount = await lendinghand.methods.postCount().call();
+        if (typeof postCount === "bigint") {
+          for (var i = 1; i <= postCount; i++) {
+            const post = await lendinghand.methods.posts(i).call();
+            setPosts((posts) => [...posts, post]);
+          }
+          setLoading(false);
+        }
+      } else {
+        window.alert(
+          "Please connect your Metamask Account to the Goerli TestNet"
+        );
       }
-      setLoading(false)
-    }
-     } else {
-        window.alert('Please connect your Metamask Account to the Goerli TestNet')
-      }
-      
-    }
+    };
 
-
-    loadBlockchainData().catch(console.error)
-
-
-
-  }, [])
-
+    loadBlockchainData().catch(console.error);
+  }, []);
 
   return (
     <div>
-      { loading ?  <p>Loading....</p> :
-          <div>
-            <h1 className='text-4xl m-4'>Discover Fundraisers</h1>
-            <div className='grid grid-rows-4 grid-cols-4 m-24'>
-              {
-            posts.map((post: any) => {
-              return (
-                <div key={post.name} className='m-4 bg-white'>
-                  <img src="IlliniBlockchain.svg"></img>
-                  <div className='flex justify-center flex-col items-center'>
-                    <p className='font-bold'>{post.name}</p>
-                    <p>Current Raised: {Number(post.current) / Math.pow(10, 18)} Eth</p>
-                    <p>Goal: {Number(post.goal) / Math.pow(10, 18)} Eth</p>
-                  </div>
+      {loading ? (
+        <div className="flex justify-center items-center min-h-screen">
+          <PulseLoader color="#3498db" size={25} />
+        </div>
+      ) : (
+        <div>
+          <h1 className="text-4xl font-semibold tracking-wider text-center text-rgb(240, 240, 240) p-5 rounded shadow-lg">
+            Discover Fundraisers
+          </h1>
+          <div className="grid grid-cols-4 gap-12 m-4">
+            {posts.map((post: any) => (
+              <div
+                key={post.name}
+                className="p-4 bg-white shadow-lg rounded-md"
+              >
+                <img
+                  src="IlliniBlockchain.svg"
+                  alt="Illini Blockchain"
+                  className="mb-4"
+                />
+                <div className="flex flex-col items-center justify-center">
+                  <p className="font-bold text-xl mb-2">{post.name}</p>
+                  <p className="text-sm mb-2">
+                    Current Raised: {Number(post.current) / Math.pow(10, 18)}{" "}
+                    Eth
+                  </p>
+                  <p className="text-sm">
+                    Goal: {Number(post.goal) / Math.pow(10, 18)} Eth
+                  </p>
                 </div>
-              )
-            })
-        }
+              </div>
+            ))}
           </div>
         </div>
-        }
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Lend
+export default Lend;
